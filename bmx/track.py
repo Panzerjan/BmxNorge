b4d28f2c9532a14ff9ed16f7d151f3217ff41ad2
Panzerjan <johannessen54@gmail.com>
2023-06-21 09:09:28 +0200

    progress

--- bmx/track.py
+++ bmx/track.py
@@ -4,6 +4,20 @@ import datetime
 file = "./files/track.json"
 
 
+def find_highest_id(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    highest_id = 0
+
+    for item in data['track']:
+        current_id = int(item['Id']) if item['Id'].isdigit() else int(item['Id'])
+        if current_id > highest_id:
+            highest_id = current_id
+
+    return highest_id
+
+
 def get_today_date():
     today = datetime.date.today()
     return today.strftime("%Y-%m-%d")
@@ -14,17 +28,21 @@ def add_track(path, tracks):
     with open(path, "r") as file:
         data = json.load(file)
 
-    # get today
+    highest_id = find_highest_id(path)
+    new_id = str(highest_id + 1)
 
+    # get today
     today_date = get_today_date()
+
     # Add each race to the data
     for track in tracks:
-        if track['TrackName'] in [r['TrackName'] for r in data['track']]:
+        if track['Club'] in [r['Club'] for r in data['track']]:
             print(
-                f"{track['TrackName']} already exists in the list of tracks.")
+                f"{track['Club']} already exists in the list of tracks.")
         else:
             data['track'].append({
-                'TrackName': track['TrackName'],
+                'Id': new_id,
+                'Club': track['Club'],
                 'location': track['location'],
                 'Country': track['Country'],
                 'Region': track['Region'],
@@ -33,15 +51,15 @@ def add_track(path, tracks):
 
 
             })
-            print(f"{track['TrackName']} has been added to the list of races.")
+            print(f"{track['Club']} has been added to the list of tracks.")
 
     # Write the updated data back to the JSON file
     with open(path, "w") as outfile:
         json.dump(data, outfile, indent=4)
 
 
-tracks = [{'TrackName': 'Bmx Verona', 'location': 'Verona',
-           'Country': 'Italy', 'Region': ' Provinsen Verona', 'address': 'Via Sogare, 1'}, {'TrackName': 'BMX Club Circuit Zolder', 'location': ' Circuit Zolder',
-                                                                                            'Country': 'Belgium', 'Region': 'Heusden-Zolder', 'address': 'Terlaemenlaan 30'}]
+tracks = [{'Club': 'Aremark BMX', 'location': 'Aremark',
+           'Country': 'Norway', 'Region': 'East', 'address': 'Asketjernveien'}, {'Club': 'BOC BMX', 'location': 'Rykkinn',
+                                                                                 'Country': 'Norway', 'Region': 'East', 'address': 'Gamle Lommedalsvei 99'}]
 
 add_track(file, tracks)
